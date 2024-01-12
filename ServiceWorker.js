e570552e38785a2c1560e7c3ebb7257b0ebf0a19
pygoose <35179439+SimLoads@users.noreply.github.com>
2024-01-12 18:51:07 +0000

    PWA Switch
    Update to PWA build for embedding.

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Bonk Developments-PRESERVE THE GAME-0A";
+const contentToCache = [
+    "Build/MOBITEST 1.12.24.loader.js",
+    "Build/MOBITEST 1.12.24.framework.js.br",
+    "Build/MOBITEST 1.12.24.data.br",
+    "Build/MOBITEST 1.12.24.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
