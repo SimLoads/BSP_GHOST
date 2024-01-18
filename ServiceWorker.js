ce8a4b5a11f42cb23be05723b5e29e668347451c
pygoose <35179439+SimLoads@users.noreply.github.com>
2024-01-18 10:54:07 +0000

    REBUILD

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Bonk Developments-PRESERVE THE GAME-0A";
+const contentToCache = [
+    "Build/BSP_GHOST.loader.js",
+    "Build/BSP_GHOST.framework.js",
+    "Build/BSP_GHOST.data",
+    "Build/BSP_GHOST.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
